ac5eea6f773c9d6409e178853b0bce6d734d29a0
Edgar <ejgallegos@espe.edu.ec>
2024-07-12 23:08:25 -0500

    feat: Add react-icons package for additional icons
    Add the react-icons package to the project to provide a wide range of additional icons for use in the application. This package will enhance the visual appeal and functionality of the UI.

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { IoClose } from "react-icons/io5";
 
-function Gallery() {
+const Gallery = () => {
+    const [data, setData] = useState([])
+    const [popup, setPopup] = useState(false)
+    const [popupData, setPopupData] = useState("")
+
+    const getData = async (count) => {
+        try {
+            const api = await fetch(`https://picsum.photos/v2/list?limit=${count}`)
+            setData(await api.json())
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getData(12)
+    }, [])
+
     return (
-        <div>Gallery</div>
+        <div className='w-full h-full bg-slate-900 flex justify-center py-2 overflow-hidden relative'>
+            <div className="rounded sm:columns-2 md:columns-3 xl:columns-4 gap-2 w-11/12 box-border">
+                {data.length > 0 ? data.map((item) => (
+                    <img
+                        key={item.id}
+                        src={item.download_url}
+                        onClick={(e) => {
+                            setPopupData(e.target.src)
+                            setPopup(true)
+                        }}
+                        alt="gallery"
+                        className='w-full mb-2 rounded-xl hover:scale-110 shadow-2xl hover:shadow-slate-900 transition'
+                    />
+                )) : "No images found"}
+            </div>
+
+            {popup && (
+                <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full bg-slate-900 rounded-md flex justify-center items-center z-40 transition-all duration-300 ease-in-out opacity-100">
+                    <IoClose
+                        className="w-16 h-16 text-white absolute right-0 top-0 z-50 cursor-pointer"
+                        onClick={() => setPopup(false)}
+                    />
+                    <img src={popupData} className="w-full sm:w-5/6 md:w-1/2" alt="popup" />
+                </div>
+            )}
+        </div>
     )
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import Gallery from './Gallery'
 
 export default function Example() {
     return (
@@ -61,6 +62,8 @@ export default function Example() {
                     />
                 </div>
             </div>
+
+            <Gallery />
         </div>
     )
 }
